6ff32e3cb4404fba1728c5a1e89d34a5849b5f44
Eric <ericbyington33@gmail.com>
2022-10-05 10:15:44 -0400

    variables added

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 var lettersU = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var lettersL= []
+var numericC = ['0','1','2','3','4','5','6','7','8','9']
+var specialK = ['!','@','#','$','%','&']
+
+
+
 for(let letter of lettersU){
   lettersL.push(letter.toLowerCase());
 }
 console.log(lettersU, lettersL);
 
 function generatePassword(){
+
+  let randomString
+  let psL = prompt("Enter password length 8- 128 characters?");
+  let Upper = confirm("Would you like to use uppercase character?");
+  let Lower = confirm("Would you like to use lowercase characters?");
 
 }
 
