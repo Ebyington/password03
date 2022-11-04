5d55a187523c981a086db54d53e02556c18635ca
Eric <ericbyington33@gmail.com>
2022-11-04 18:58:09 -0400

    random Math

--- Develop/script.js
+++ Develop/script.js
@@ -15,6 +15,13 @@ var specialK = ['!','@','#','$','%','&']
 //   lettersL.push(letter.toLowerCase());
 // // }
 // console.log(lettersU, lettersL);
+function randomNumber(min, max) {
+  return Math.floor(Math.random()*(max - min) + min)
+}
+
+function getItem(list) {
+  return list[randomNumber(0, list.length - 1 )]
+}
 
 function generatePassword() {
   var userInput = window.prompt("Enter password length 8-128 characters?")
@@ -25,17 +32,37 @@ function generatePassword() {
   } else {
     
   }
+//  boolean variables for password parameters with modal confirm window
+var userInNum = window.confirm("Would you like to use numbers?")
+var userInUppercase = window.confirm("Would you like to use Uppercase characters?")
+var userInLowercase = window.confirm("Would you like to use lowercase characters?")
+var userInSymbol = window.confirm("Would you like to use symbols?")
+var passParam = []
 
-  var userInNum = window.confirm("Would you like to use numbers?")
-  var userInUppercase = window.confirm("Would you like to use Uppercase characters?")
-  var userInLowercase = window.confirm("Would you like to use lowercase characters?")
-  var userInSymbol = window.confirm("Would you like to use symbols?")
-  
   for (var i = 0; i < lettersU.length; i++) {
     lettersL[i] = lettersU[i].toLowerCase()
   }
+  if (userInNum === true) {
+    passParam.push(userInNum)
+  } 
+  if (userInLowercase === true) {
+    passParam.push(userInLowercase)
+  }
+  if (userInUppercase === true) {
+    passParam.push(userInUppercase) 
+  }
+  if (userInSymbol === true) {
+    passParam.push(userInSymbol)
+  }
+  console.log(passParam)
+  var newPassword = ""
+  
+  for (var i = 0; i < pwLength; i ++) {
+    var randomList = getItem(passParam)
+    var randomCharacter = getItem(randomList)
+    console.log(randomCharacter)
+  }
 }
-
 
 // Write password to the #password input
 function writePassword() {
